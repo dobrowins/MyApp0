6ee2f2657e2b56812a05c3997ec30468ded4b775
dobrowins <dobrowins@gmail.com>
2016-04-23 12:32:24 +0300

    comment test

--- app/src/main/java/com/example/anon/myapp0/My_Activity.java
+++ app/src/main/java/com/example/anon/myapp0/My_Activity.java
@@ -28,6 +28,8 @@ public class My_Activity extends AppCompatActivity {
         });
     }
 
+    //testing desktop github
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
