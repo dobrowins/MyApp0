6a81d829f080e435c31b6877b5732404a313a9c6
dobrowins <dobrowins@gmail.com>
2016-05-03 11:54:24 +0300

    1st app done!

--- app/src/main/java/com/example/anon/myapp0/My_Activity.java
+++ app/src/main/java/com/example/anon/myapp0/My_Activity.java
@@ -5,9 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class My_Activity extends AppCompatActivity {
 
@@ -27,6 +29,14 @@ public class My_Activity extends AppCompatActivity {
             }
         });
     }
+
+    public void topClick(View v) {
+        Toast.makeText(this, "Top button clicked", Toast.LENGTH_SHORT).show();
+    }
+    public void bottomClick(View v) {
+        Toast.makeText(this, "Bottom button clicked", Toast.LENGTH_LONG).show();
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/example/anon/myapp0/helloworld.java
+++
@@ -1,5 +0,0 @@
-class helloworld {
-    public static void hello() {
-        System.out.println("Hello, world!");
-    }
-}
